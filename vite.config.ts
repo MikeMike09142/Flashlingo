95d026d128cca7335d2c0fcd294bb30ac17c8b68
MikeMike09142 <onlinekore874@gmail.com>
2025-07-25 11:51:25 -0500

    deleating j.son

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export defineConfig({
   plugins: [
     react(),
     VitePWA({
@@ -22,13 +22,13 @@ export default defineConfig({
             src: '/icons/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: ['any', 'maskable']
           },
           {
             src: '/icons/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: ['any', 'maskable']
           }
         ]
       },
